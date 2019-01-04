2c537f19e554ef67bbd6a1270432db4e7e2dcf29
Ryosuke <ryosuke.san.hana@gmail.com>
2019-01-04 15:49:15 -0800

    :sparkles: Added index for exporting all components

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export Base from "./components/Base/Base";
+export Button from "./components/Button/Button";
+export Form from "./components/Form/Form";
+export Grid from "./components/Grid/Grid";
+export Menu from "./components/Menu/Menu";
+export Table from "./components/Table/Table";
