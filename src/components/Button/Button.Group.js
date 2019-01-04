0ff81ebacc3cb92faae5971ab0f983351836c153
Ryosuke <ryosuke.san.hana@gmail.com>
2019-01-04 15:32:22 -0800

    :bulb: fixed jsdocs for styleguidist

--- src/components/Button/Button.Group.js
+++ src/components/Button/Button.Group.js
@@ -2,13 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Group = ({ className, children, ariaLabel }) => (
+const BaseGroup = ({ className, children, ariaLabel }) => (
   <div className={className} role="group" ariaLabel={ariaLabel}>
     {children}
   </div>
 );
 
-const StyledGroup = styled(Group)`
+/**
+ * Groups buttons together and provides aria role `group`
+ */
+const Group = styled(BaseGroup)`
   letter-spacing: -0.31em; /* Webkit: collapse white-space between units */
   *letter-spacing: normal; /* reset IE < 8 */
   *word-spacing: -0.43em; /* IE < 8: collapse white-space between units */
@@ -27,4 +30,16 @@ const StyledGroup = styled(Group)`
   }
 `;
 
-export default StyledGroup;
+Group.propTypes = {
+
+  /**
+   * Child components (array or single element)
+   */
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+};
+
+/** @component */
+export default Group;
