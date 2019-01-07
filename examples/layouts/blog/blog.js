fe13669d53ab16309d768e73bea5ec38ccc59733
Ryosuke <ryosuke.san.hana@gmail.com>
2019-01-06 19:15:21 -0800

    :bug: Fixed structure for React (closed DOM elems)

--- examples/layouts/blog/blog.js
+++ examples/layouts/blog/blog.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 import { createGlobalStyle } from "styled-components";
 
-import Base from "../../../src/components/Base/Base"
-import Grid from "../../../src/components/Grid/Grid"
-import Button from "../../../src/components/Button/Button"
-import Menu from "../../../src/components/Menu/Menu"
+import Base from "../../../src/components/Base/Base";
+import Grid from "../../../src/components/Grid/Grid";
+import Button from "../../../src/components/Button/Button";
+import Menu from "../../../src/components/Menu/Menu";
 
 const BlogTheme = createGlobalStyle`
   * {
@@ -191,146 +191,247 @@ h3 {
 export default function blog() {
   return (
     <Base>
-        <StyleguideTheming />
-    <Grid id="layout">
-    <Grid.Unit width={[1, 1/4]}> <div class="header">
-        <h1 class="brand-title">A Sample Blog</h1>
-        <h2 class="brand-tagline">Creating a blog layout using Pure</h2>
+      <BlogTheme />
+      <Grid id="layout">
+        <Grid.Unit width={[1, 1 / 4]}>
+          <div className="header">
+            <h1 className="brand-title">A Sample Blog</h1>
+            <h2 className="brand-tagline">Creating a blog layout using Pure</h2>
 
-        <nav class="nav">
-            <ul class="nav-list">
-                <li class="nav-item">
-                    <Button as="a" href="http://purecss.io">Pure</Button>
+            <nav className="nav">
+              <ul className="nav-list">
+                <li className="nav-item">
+                  <a href="http://purecss.io">
+                    <Button>Pure</Button>
+                  </a>
                 </li>
-                <li class="nav-item">
-                    <Button as="a" href="http://yuilibrary.com">YUI Library</Button>
+                <li className="nav-item">
+                  <Button as="a" href="http://yuilibrary.com">
+                    YUI Library
+                  </Button>
                 </li>
-            </ul>
-        </nav>
-        </div>
-    </Grid.Unit>
+              </ul>
+            </nav>
+          </div>
+        </Grid.Unit>
 
-    <Grid.Unit width={[1, 1/4]} class="content">
-        <div>
-            {/* A wrapper for all the blog posts */}
-            <div class="posts">
-                <h1 class="content-subhead">Pinned Post</h1>
+        <Grid.Unit width={[1, 1 / 4]} className="content">
+          <div>
+            <div className="posts">
+              <h1 className="content-subhead">Pinned Post</h1>
 
-                {/*  A single blog post */}
-                <section class="post">
-                    <header class="post-header">
-                        <img width="48" height="48" alt="Tilo Mitra&#x27;s avatar" class="post-avatar" src="img/common/tilo-avatar.png">
+              <section className="post">
+                <header className="post-header">
+                  <img
+                    width="48"
+                    height="48"
+                    alt="Tilo Mitra&#x27;s avatar"
+                    className="post-avatar"
+                    src="img/common/tilo-avatar.png"
+                  />
 
-                        <h2 class="post-title">Introducing Pure</h2>
+                  <h2 className="post-title">Introducing Pure</h2>
 
-                        <p class="post-meta">
-                            By <a href="#" class="post-author">Tilo Mitra</a> under <a class="post-category post-category-design"
-                                href="#">CSS</a> <a class="post-category post-category-pure" href="#">Pure</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <p>
-                            Yesterday at CSSConf, we launched Pure – a new CSS library. Phew! Here are the <a href="https://speakerdeck.com/tilomitra/pure-bliss">slides
-                                from the presentation</a>. Although it looks pretty minimalist, we’ve been working on
-                            Pure for several months. After many iterations, we have released Pure as a set of small,
-                            responsive, CSS modules that you can use in every web project.
-                        </p>
-                    </div>
-                </section>
-            </div>
-
-            <div class="posts">
-                <h1 class="content-subhead">Recent Posts</h1>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img width="48" height="48" alt="Eric Ferraiuolo&#x27;s avatar" class="post-avatar" src="img/common/ericf-avatar.png">
-
-                        <h2 class="post-title">Everything You Need to Know About Grunt</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Eric Ferraiuolo</a> under <a class="post-category post-category-js"
-                                href="#">JavaScript</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                    </div>
-                </section>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img width="48" height="48" alt="Reid Burke&#x27;s avatar" class="post-avatar" src="img/common/reid-avatar.png">
-
-                        <h2 class="post-title">Photos from CSSConf and JSConf</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Reid Burke</a> under <a class="post-category" href="#">Uncategorized</a>
-                        </p>
-                    </header>
-
-                    <Grid.Unit class="post-description">
-                        <Grid class="post-images">
-                            <Grid.Unit width={[1, 1/2]}> <a href="http://www.flickr.com/photos/uberlife/8915936174/">
-                                <img alt="Photo of someone working poolside at a resort" class="pure-img-responsive"
-                                    src="http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg">
-                                </a>
-
-                                <div class="post-image-meta">
-                                    <h3>CSSConf Photos</h3>
-                                </div>
-                            </Grid.Unit>
-
-                            <Grid.Unit width={[1, 1/2]}> <a href="http://www.flickr.com/photos/uberlife/8907351301/">
-                                <img alt="Photo of the sunset on the beach" class="pure-img-responsive" src="http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg">
-                                </a>
-
-                                <div class="post-image-meta">
-                                    <h3>JSConf Photos</h3>
-                                </div>
-                            </Grid.Unit>
-                        </Grid>
-            </div>
-            </section>
-
-            <section class="post">
-                <header class="post-header">
-                    <img width="48" height="48" alt="Andrew Wooldridge&#x27;s avatar" class="post-avatar" src="img/common/andrew-avatar.png">
-
-                    <h2 class="post-title">YUI 3.10.2 Released</h2>
-
-                    <p class="post-meta">
-                        By <a class="post-author" href="#">Andrew Wooldridge</a> under <a class="post-category post-category-yui"
-                            href="#">YUI</a>
-                    </p>
+                  <p className="post-meta">
+                    By
+                    {" "}
+                    <a href="#" className="post-author">
+                      Tilo Mitra
+                    </a>
+                    {" "}
+                    under
+                    {" "}
+                    <a className="post-category post-category-design" href="#">
+                      CSS
+                    </a>
+                    {" "}
+                    <a className="post-category post-category-pure" href="#">
+                      Pure
+                    </a>
+                  </p>
                 </header>
 
-                <div class="post-description">
-                    <p>
-                        We are happy to announce the release of YUI 3.10.2! You can find it now on the Yahoo! CDN,
-                        download it directly, or pull it in via npm. We’ve also updated the YUI Library website
-                        with the latest documentation.
-                    </p>
+                <div className="post-description">
+                  <p>
+                    Yesterday at CSSConf, we launched Pure – a new CSS library.
+                    Phew! Here are the
+                    {" "}
+                    <a href="https://speakerdeck.com/tilomitra/pure-bliss">
+                      slides from the presentation
+                    </a>
+                    . Although it looks pretty minimalist, we’ve been working on
+                    Pure for several months. After many iterations, we have
+                    released Pure as a set of small, responsive, CSS modules
+                    that you can use in every web project.
+                  </p>
                 </div>
-            </section>
-        </div>
+              </section>
+            </div>
 
-        <footer class="footer">
-            <Menu as="ul" horizontal>
-                <Menu.Item as="li"><a href="http://purecss.io/" class="link">About</a></Menu.Item>
-                <Menu.Item as="li"><a href="http://twitter.com/yuilibrary/" class="link">Twitter</a></Menu.Item>
-                <Menu.Item as="li"><a href="http://github.com/pure-css/pure/" class="link">GitHub</a></Menu.Item>
-            </Menu>
+            <div className="posts">
+              <h1 className="content-subhead">Recent Posts</h1>
+
+              <section className="post">
+                <header className="post-header">
+                  <img
+                    width="48"
+                    height="48"
+                    alt="Eric Ferraiuolo&#x27;s avatar"
+                    className="post-avatar"
+                    src="img/common/ericf-avatar.png"
+                  />
+
+                  <h2 className="post-title">
+                    Everything You Need to Know About Grunt
+                  </h2>
+
+                  <p className="post-meta">
+                    By
+                    {" "}
+                    <a className="post-author" href="#">
+                      Eric Ferraiuolo
+                    </a>
+                    {" "}
+                    under
+                    {" "}
+                    <a className="post-category post-category-js" href="#">
+                      JavaScript
+                    </a>
+                  </p>
+                </header>
+
+                <div className="post-description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur.
+                  </p>
+                </div>
+              </section>
+
+              <section className="post">
+                <header className="post-header">
+                  <img
+                    width="48"
+                    height="48"
+                    alt="Reid Burke&#x27;s avatar"
+                    className="post-avatar"
+                    src="img/common/reid-avatar.png"
+                  />
+
+                  <h2 className="post-title">Photos from CSSConf and JSConf</h2>
+
+                  <p className="post-meta">
+                    By
+                    {" "}
+                    <a className="post-author" href="#">
+                      Reid Burke
+                    </a>
+                    {" "}
+                    under
+                    {" "}
+                    <a className="post-category" href="#">
+                      Uncategorized
+                    </a>
+                  </p>
+                </header>
+
+                <Grid.Unit className="post-description">
+                  <Grid class="post-images">
+                    <Grid.Unit width={[1, 1 / 2]}>
+                      {" "}
+                      <a href="http://www.flickr.com/photos/uberlife/8915936174/">
+                        <img
+                          alt="Someone working poolside at a resort"
+                          className="pure-img-responsive"
+                          src="http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg"
+                        />
+                      </a>
+                      <div className="post-image-meta">
+                        <h3>CSSConf Photos</h3>
+                      </div>
+                    </Grid.Unit>
+
+                    <Grid.Unit width={[1, 1 / 2]}>
+                      {" "}
+                      <a href="http://www.flickr.com/photos/uberlife/8907351301/">
+                        <img
+                          alt="The sunset on the beach"
+                          className="pure-img-responsive"
+                          src="http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg"
+                        />
+                      </a>
+                      <div className="post-image-meta">
+                        <h3>JSConf Photos</h3>
+                      </div>
+                    </Grid.Unit>
+                  </Grid>
+                </Grid.Unit>
+              </section>
+
+              <section className="post">
+                <header className="post-header">
+                  <img
+                    width="48"
+                    height="48"
+                    alt="Andrew Wooldridge&#x27;s avatar"
+                    className="post-avatar"
+                    src="img/common/andrew-avatar.png"
+                  />
+
+                  <h2 className="post-title">YUI 3.10.2 Released</h2>
+
+                  <p className="post-meta">
+                    By
+                    {" "}
+                    <a className="post-author" href="#">
+                      Andrew Wooldridge
+                    </a>
+                    {" "}
+                    under
+                    {" "}
+                    <a className="post-category post-category-yui" href="#">
+                      YUI
+                    </a>
+                  </p>
+                </header>
+
+                <div className="post-description">
+                  <p>
+                    We are happy to announce the release of YUI 3.10.2! You can
+                    find it now on the Yahoo! CDN, download it directly, or pull
+                    it in via npm. We’ve also updated the YUI Library website
+                    with the latest documentation.
+                  </p>
+                </div>
+              </section>
+            </div>
+          </div>
+        </Grid.Unit>
+
+        <footer className="footer">
+          <Menu as="ul" horizontal>
+            <Menu.Item as="li">
+              <a href="http://purecss.io/" className="link">
+                About
+              </a>
+            </Menu.Item>
+            <Menu.Item as="li">
+              <a href="http://twitter.com/yuilibrary/" className="link">
+                Twitter
+              </a>
+            </Menu.Item>
+            <Menu.Item as="li">
+              <a href="http://github.com/pure-css/pure/" className="link">
+                GitHub
+              </a>
+            </Menu.Item>
+          </Menu>
         </footer>
-    </Grid.Unit>
-</Grid>
-</Base>
-  )
+      </Grid>
+    </Base>
+  );
 }
